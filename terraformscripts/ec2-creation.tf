8b5e93834f63a5acaf3cf3012dcf78214347880e
Gaurav Kumar <78254550+technicalgtufgu@users.noreply.github.com>
2025-09-15 04:13:44 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,14 +1,13 @@
-provider "aws"{
-region = "ap-south-1"
+provider "aws" {
+    region = "us-east-1"
 }
 
-
-resource "aws_instance"  "AWSInstance" {
- ami = "ami-0b32d400456908bf9"
- instance_type = "t2.micro"
- key_name = "mithuntechnologiesdevopsaws2025"
- security_groups = ["launch-wizard-2"]
- tags = {
- Name = "terraformserver"
- }
+resource "aws_instance" "AWSEC2Instance" {
+    ami = "ami-0b09ffb6d8b58ca91"
+    instance_type = "t2.micro"
+    key_name = "ansible-key.pem"
+    security_groups = [launch-wizard-1]
+    tags = {
+        Name = "terraformserver"
+    }
 }
