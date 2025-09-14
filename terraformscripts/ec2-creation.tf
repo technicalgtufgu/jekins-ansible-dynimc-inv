c621d20f698b69778c8bce9b07b3e983aa427e37
Gaurav Kumar <78254550+technicalgtufgu@users.noreply.github.com>
2025-09-15 04:44:49 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -6,7 +6,8 @@ resource "aws_instance" "AWSEC2Instance" {
     ami = "ami-0b09ffb6d8b58ca91"
     instance_type = "t2.micro"
     key_name = "ansible-key"
-    security_groups = ["eksctl-demo-cluster-cluster-ClusterSharedNodeSecurityGroup-iAMQtf2P9brC"]
+    vpc_security_group_ids = ["sg-0d90927112521762b"]
+    subnet_id = "subnet-0d9d0759549596317" 
     tags = {
         Name = "terraformserver"
     }
